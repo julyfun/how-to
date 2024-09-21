361da1d4e1751521a209ef7544cfdcd7c58e6f9b
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-09-21 20:29:48 +0800

    ?? notes/julyfun/工作/william/240921/

---
+++ notes/julyfun/工作/william/240921/usaco-2021-january-contest-silver-problem-3-spaced-out.py
@@ -0,0 +1,19 @@
+n = int(input())
+f = [[0, 0]] * n
+v = [[0, 0]] * n
+for i in range(n):
+    s = list(map(int, input().split()))
+    for j, x in enumerate(s):
+        f[j][(i + 1) % 2] += x
+        v[i][(j + 1) % 2] += x
+print(f)
+print(v)
+ans1 = 0
+for x, y in f:
+    ans1 += max(x, y)
+ans2 = 0
+for x, y in v:
+    ans2 += max(x, y)
+print(max(ans1, ans2))
+
+
