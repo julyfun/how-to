3ed6586f87cd888a63a067df7ec172185cca1719
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-11-09 21:13:12 +0800

    M notes/julyfun/工作/william/241109/p6111-usaco18jan-mootube-s.py

--- notes/julyfun/工作/william/241109/p6111-usaco18jan-mootube-s.py
+++ notes/julyfun/工作/william/241109/p6111-usaco18jan-mootube-s.py
@@ -1,9 +1,14 @@
-def dfs(u, fa, k):
+def dfs(u, fa, k, depth=0):
     global cnt
     for v, w in G[u]:
         if v != fa and w >= k:
             cnt += 1
-            dfs(v, u, k)
+            dfs(v, u, k, depth + 1)
+
+import sys
+# sys.stdin = open('/Users/florian/Downloads/P6111_8.in', 'r')
+# Increase the recursion limit
+sys.setrecursionlimit(10000)
 
 n, Q = map(int, input().split())
 G = [[] for _ in range(n + 1)]
