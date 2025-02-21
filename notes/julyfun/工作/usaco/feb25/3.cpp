79024e75d468efcddce18e6da0902023daa401e7
4070s wsl julyfun <julyfun.collect@outlook.com>
2025-02-21 22:20:16 +0800

    M notes/julyfun/工作/usaco/feb25/3.cpp | ?? notes/julyfun/工作/usaco/feb25/3-a.cpp

--- notes/julyfun/工作/usaco/feb25/3.cpp
+++ notes/julyfun/工作/usaco/feb25/3.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 const int N = 1e2 + 10;
 int a[N];
-int last_cont_start[N];
 int main() {
     int t; cin >> t;
     while (t--) {
@@ -10,36 +9,6 @@ int main() {
         cin >> n >> k;
         for (int i = 1; i <= n; i++)
             cin >> a[i];
-        for (int i = 1; i <= k; i++)
-            last_cont_start[i] = 0;
-        bool ok = true;
-        int p = 1;
-        while (p <= n && ok) {
-            if (p >= 2 && a[p] == a[p - 1]) {
-                p++;
-                continue;
-            }
-            int x = last_cont_start[a[p]];
-            last_cont_start[a[p]] = p;
-            if (x != 0) {
-                int rep = p - x;
-                for (int i = 1; i <= rep; i++) {
-                    if (a[p] != a[p - rep] || p > n) {
-                        ok = false;
-                        break;
-                    }
-                    p++;
-                }
-            }
-            else {
-                p++;
-            }
-        }
-        if (ok)
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
     }
-
     return 0;
 }
