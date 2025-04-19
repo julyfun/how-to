dc1db95492569979188f61e7d6f6b202558e311a
4070s wsl julyfun <julyfun.collect@outlook.com>
2025-04-19 20:48:11 +0800

    M notes/julyfun/工作/kedaya/250419-循环与分支/p1122.cpp

--- notes/julyfun/工作/kedaya/250419-循环与分支/p1122.cpp
+++ notes/julyfun/工作/kedaya/250419-循环与分支/p1122.cpp
@@ -1,19 +1,25 @@
-// t4
 #include <bits/stdc++.h>
 using namespace std;
 int a[100010];
 int main() {
-    int n; cin >> n;
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    int t;
-    cin >> t;
-    for (int i = 1; i <= n; i++) {
-        if (a[i] == t) {
-            cout << i << endl;
-            break;
-        }
-    }
-    cout << -1 << endl;
-    return 0;
+	// 先读入 n，再读入 n 个数 
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	// 读入 x，并寻找 x 在 a 数组中第一次出现的位置
+	int x, flag = 0;
+	cin >> x;
+	for (int i = 1; i <= n; i++) {
+		if (x == a[i]) {
+			cout << i << endl;
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		cout << "-1";
+	}
+	return 0;
 }
