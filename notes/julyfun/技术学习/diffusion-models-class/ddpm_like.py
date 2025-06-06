a163e22ebb72a0b5bfecd3c0eab5d6d335741cbf
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-07 05:07:56 +0800

    M notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py

--- notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py
+++ notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py
@@ -23,6 +23,21 @@ from torchvision.utils import make_grid
 x, y = next(iter(train_loader))
 print(f"x.shape: {x.shape}, y.shape: {y.shape}")    
 plt.imshow(make_grid(x)[0], cmap="Greys")
-type(make_grid(x).shape)
+print(make_grid(x).shape)
+
+# %%
+def corrupt(x, amount): # amount of shape (B,)
+    noise = torch.rand_like(x)
+    amount = amount.view(-1, 1, 1, 1)
+    return noise * amount + x * (1 - amount)
+
+# %%
+amount = torch.linspace(0, 1, x.shape[0])
+print(amount.shape, amount) # torch.size([8])
+noised_x = corrupt(x, amount)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+axs[0].imshow(make_grid(x)[0], cmap="Greys")
+axs[1].imshow(make_grid(noised_x)[0], cmap="Greys")
 
 # %%
