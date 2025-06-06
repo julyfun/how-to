66d3b79476acf5490d5b03c0589dd9b920594291
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-07 04:47:43 +0800

    M notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py

---
+++ notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py
@@ -0,0 +1,12 @@
+# %%
+import torch
+import torchvision
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from diffusers import DDPMScheduler, UNet2DModel
+from matplotlib import pyplot as plt
+
+device = torch.device("cuda")
+print(f"Using device: {device}")
+# %%
