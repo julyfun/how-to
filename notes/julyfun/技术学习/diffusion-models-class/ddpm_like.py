0f01975ec58413c0361dfdde1ab0fa3b46618ce4
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-07 04:57:49 +0800

    M .gitignore | M notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py

--- notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py
+++ notes/julyfun/技术学习/diffusion-models-class/ddpm_like.py
@@ -10,3 +10,19 @@ from matplotlib import pyplot as plt
 device = torch.device("cuda")
 print(f"Using device: {device}")
 # %%
+
+dataset = torchvision.datasets.MNIST(root="ignoreme-mnist/", train=True, download=True, transform=torchvision.transforms.ToTensor())
+
+# %%
+
+train_loader = DataLoader(dataset, batch_size=8, shuffle=True)
+
+# %%
+
+from torchvision.utils import make_grid 
+x, y = next(iter(train_loader))
+print(f"x.shape: {x.shape}, y.shape: {y.shape}")    
+plt.imshow(make_grid(x)[0], cmap="Greys")
+type(make_grid(x).shape)
+
+# %%
