33e85740a95c2929bd2fc3dce266094d14c26fc6
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-08 21:14:19 +0800

    M notes/julyfun/技术学习/diffusion-models-class/02-diffusion_finetuning.py

--- notes/julyfun/技术学习/diffusion-models-class/02-diffusion_finetuning.py
+++ notes/julyfun/技术学习/diffusion-models-class/02-diffusion_finetuning.py
@@ -24,3 +24,22 @@ DDPMPipeline.__mro__
 # Generate images using ()
 imgs = image_pipe().images
 imgs[0]
+
+# %%
+
+scheduler = DDIMScheduler.from_pretrained('google/ddpm-celebahq-256')
+
+scheduler.set_timesteps(num_inference_steps=40)
+# %%
+
+scheduler.timesteps # 输出 like [975, 950, ...]
+
+# %%
+from tqdm import tqdm
+
+x = torch.randn((4, 3, 256, 256), device=device)
+
+for i, t in tqdm(enumerate(scheduler.timesteps)):
+    input = scheduler.scale_model_input(x, t)
+    with torch.no_grad():
+        noise_pred = image_pipe.unet(input, t)["sample"]
