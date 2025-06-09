b9d52078cc370c72840eaf1b03ae01c42a9d53bb
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-10 01:55:37 +0800

    ?? notes/julyfun/技术学习/diffusion-models-class/unit2-02_class_conditioned_diffusion_model_example.md |

---
+++ notes/julyfun/技术学习/diffusion-models-class/unit2-02_class_conditioned_diffusion_model_example.py
@@ -0,0 +1,14 @@
+# %%
+import torch
+import torchvision
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from diffusers import DDPMScheduler, UNet2DModel
+from matplotlib import pyplot as plt
+from tqdm.auto import tqdm
+
+device = 'cuda'
+print(f'Using device: {device}')
+
+# %%
