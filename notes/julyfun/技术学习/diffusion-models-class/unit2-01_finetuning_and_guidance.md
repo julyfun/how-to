5395eb01e143676e1a38fe5098d2365a4d995296
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-10 01:45:31 +0800

    D notes/julyfun/技术学习/diffusion-models-class/1-02-手写.md | RM notes/julyfun/技术学习/diffusion-models-clas

---
+++ notes/julyfun/技术学习/diffusion-models-class/unit2-01_finetuning_and_guidance.md
@@ -0,0 +1,57 @@
+Generating process:
+
+```python
+x = torch.randn(4, 3, 256, 256).to(device)
+for i, t in tqdm(enumerate(scheduler.timesteps)):
+    model_input = scheduler.scale_model_input(x, t)
+    with torch.no_grad():
+        noise_pred = image_pipe.unet(model_input, t)["sample"]
+    x = scheduler.step(noise_pred, t, sample=x).prev_sample
+```
+
+## Guidance
+
+```python
+x = torch.randn(4, 3, 256, 256).to(device)
+for i, t in tqdm(enumerate(scheduler.timesteps)):
+    x = x.detach().requires_grad_()
+    model_input = scheduler.scale_model_input(x, t)
+    noise_pred = image_pipe.unet(model_input, t)["sample"]
+
+    x0 = scheduler.step(noise_pred, t, x).pred_original_sample
+    loss = <custom_loss>(x0) * <guidance_loss_scale>
+    cond_grad = -torch.autograd.grad(loss, x)[0]
+    x = x.detach() + cond_grad
+
+    x = scheduler.step(noise_pred, t, x).prev_sample
+```
+
+## CLIP Guidance
+
+```python
+
+with torch.no_grad():
+    text_features = clip_model.encode_text(text)
+
+for i, t in tqdm(enumerate(scheduler.timesteps)):
+    # print(i, t) # (1, tensor(1000)), (2, tensor(980))...
+    model_input = scheduler.scale_model_input(x, t) # DDIM loaded
+    with torch.no_grad():
+        # image_pipe is loaded by the same name
+        noise_pred = image_pipe.unet(model_input, t)["sample"]
+    cond_grad = 0
+    for cut in range(n_cuts):
+        x = x.detach().requires_grad_()
+        x0  = scheduler.step(noise_pred,t, sample=x).pred_original_sample
+        loss = <clip_loss>(x0, text_features) * guidance_scale
+        cond_grad -= torch.autograd.grad(loss, x)[0] / n_cuts
+
+    if i % 25 == 0:
+        print(f"Steps {i} loss: {loss.item()}")
+
+    alpha_bar = scheduler.alphas_cumprod[i]
+    # `alpha_bar` here is decreasing and works for textures.
+    # Can be changed to some increasing coefficients!
+    x = x.detach() + cond_grad * alpha_bar.sqrt() 
+    x = scheduler.step(noise_pred, t, x).prev_sample
+```
--- notes/julyfun/技术学习/diffusion-models-class/unit2-01_finetuning_and_guidance.py
+++ notes/julyfun/技术学习/diffusion-models-class/unit2-01_finetuning_and_guidance.py
@@ -1,14 +1,18 @@
 # %%
-
+# [utils]
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from diffusers import DDIMScheduler, DDPMPipeline
+from tqdm import tqdm
+import torchvision
+from PIL import Image
+from matplotlib import pyplot as plt
 device = 'cuda'
+make_grid = torchvision.utils.make_grid
 
 # %%
-from diffusers import DDIMScheduler, DDPMPipeline
 
 image_pipe = DDPMPipeline.from_pretrained('google/ddpm-celebahq-256')
 
@@ -35,11 +39,8 @@ scheduler.set_timesteps(num_inference_steps=40)
 
 scheduler.timesteps # 输出 like [975, 950, ...]
 
+
 # %%
-from tqdm import tqdm
-import torchvision
-from matplotlib import pyplot as plt
-make_grid = torchvision.utils.make_grid
 
 x = torch.randn((4, 3, 256, 256), device=device)
 
@@ -266,3 +267,136 @@ def color_loss(images, target_color=(0.1, 0.9, 0.5)):
     ] # None 位置会插入维度为 1
     error = torch.abs(images - target).mean() # 得到一个数
     return error
+
+# %%
+
+import open_clip
+
+clip_model, _, preprocess = open_clip.create_model_and_transforms(
+    "ViT-B-32", pretrained="openai"
+)
+
+clip_model.to(device)
+
+# %%
+
+tfms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.RandomResizedCrop(224),  # Random CROP each time
+        torchvision.transforms.RandomAffine(
+            5
+        ),  # One possible random augmentation: skews the image
+        torchvision.transforms.RandomHorizontalFlip(),  # You can add additional augmentations if you like
+        torchvision.transforms.Normalize(
+            mean=(0.48145466, 0.4578275, 0.40821073),
+            std=(0.26862954, 0.26130258, 0.27577711),
+        ),
+    ]
+)
+
+# %%
+
+def clip_loss(image, text_features):
+    image_features = clip_model.encode_image(tfms(image)) # of shape (bs, features_dim)
+    input_normed = torch.nn.functional.normalize(image_features.unsqueeze(1), dim=2)
+    embed_normed = torch.nn.functional.normalize(text_features.unsqueeze(0), dim=2)
+    dists = (
+        input_normed.sub(embed_normed).norm(dim=2).div(2).arcsin().pow(2).mul(2)
+    )
+    return dists.mean()
+
+
+# %%
+
+prompt = "Fog, forests, blue sky"
+
+guidance_scale = 8
+n_cuts = 8
+
+scheduler.set_timesteps(50)
+
+text = open_clip.tokenize([prompt]).to(device)
+
+with torch.no_grad():
+    text_features = clip_model.encode_text(text)
+    print(f"text_features.shape: {text_features.shape}") # [1, 512]
+
+x = torch.randn(4, 3, 256, 256).to(device)
+
+# %%
+
+for i, t in tqdm(enumerate(scheduler.timesteps)):
+    # print(i, t) # (1, tensor(1000)), (2, tensor(980))...
+    model_input = scheduler.scale_model_input(x, t) # DDIM loaded
+    with torch.no_grad():
+        # image_pipe is loaded by the same name
+        noise_pred = image_pipe.unet(model_input, t)["sample"]
+
+    cond_grad = 0
+    for cut in range(n_cuts):
+        x = x.detach().requires_grad_()
+
+        x0  = scheduler.step(noise_pred,t, sample=x).pred_original_sample
+
+        loss = clip_loss(x0, text_features) * guidance_scale
+
+        cond_grad -= torch.autograd.grad(loss, x)[0] / n_cuts
+
+    if i % 25 == 0:
+        print(f"Steps {i} loss: {loss.item()}")
+
+    alpha_bar = scheduler.alphas_cumprod[i]
+    x = x.detach() + cond_grad * alpha_bar.sqrt() # Diffusion coefficient
+    x = scheduler.step(noise_pred, t, x).prev_sample
+
+# %%
+grid = make_grid(x.detach(), nrow=4)
+im = grid.permute(1, 2, 0).cpu().clip(-1, 1) * 0.5 + 0.5
+Image.fromarray(np.array(im * 255).astype(np.uint8))
+
+# %%
+
+import gradio as gr
+from PIL import Image, ImageColor
+
+
+# The function that does the hard work
+def generate(color, guidance_loss_scale):
+    target_color = ImageColor.getcolor(color, "RGB")  # Target color as RGB
+    target_color = [a / 255 for a in target_color]  # Rescale from (0, 255) to (0, 1)
+    x = torch.randn(1, 3, 256, 256).to(device)
+    for i, t in tqdm(enumerate(scheduler.timesteps)):
+        model_input = scheduler.scale_model_input(x, t)
+        with torch.no_grad():
+            noise_pred = image_pipe.unet(model_input, t)["sample"]
+        x = x.detach().requires_grad_()
+        x0 = scheduler.step(noise_pred, t, x).pred_original_sample
+        loss = color_loss(x0, target_color) * guidance_loss_scale
+        cond_grad = -torch.autograd.grad(loss, x)[0]
+        x = x.detach() + cond_grad
+        x = scheduler.step(noise_pred, t, x).prev_sample
+    grid = torchvision.utils.make_grid(x, nrow=4)
+    im = grid.permute(1, 2, 0).cpu().numpy().clip(-1, 1) * 0.5 + 0.5
+    im = Image.fromarray(np.array(im * 255).astype(np.uint8))
+    im.save("ignoreme-test.jpeg")
+    return im
+
+
+# See the gradio docs for the types of inputs and outputs available
+inputs = [
+    gr.ColorPicker(label="color", value="#55FFAA"),  # Add any inputs you need here
+    gr.Slider(label="guidance_scale", minimum=0, maximum=30, value=3),
+]
+outputs = gr.Image(label="result")
+
+# And the minimal interface
+demo = gr.Interface(
+    fn=generate,
+    inputs=inputs,
+    outputs=outputs,
+    examples=[
+        ["#BB2266", 3],
+        ["#44CCAA", 5],  # You can provide some example inputs to get people started
+    ],
+)
+demo.launch(debug=True)  # debug=True allows you to see errors and output in Colab
