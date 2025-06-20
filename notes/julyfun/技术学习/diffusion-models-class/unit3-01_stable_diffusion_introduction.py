7a4a295c2effc5b4b97b4f6a4952631b9ec69dc7
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-06-20 18:59:15 +0800

    M notes/julyfun/技术学习/diffusion-models-class/unit3-01_stable_diffusion_introduction.py

--- notes/julyfun/技术学习/diffusion-models-class/unit3-01_stable_diffusion_introduction.py
+++ notes/julyfun/技术学习/diffusion-models-class/unit3-01_stable_diffusion_introduction.py
@@ -1,3 +1,4 @@
+# %%
 import torch
 import requests
 from PIL import Image
@@ -12,3 +13,21 @@ from diffusers import (
 def download_image(url):
     response = requests.get(url)
     return Image.open(BytesIO(response.content)).convert("RGB")
+
+img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
+mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo_mask.png"
+
+img_height = 512
+img_width = 512
+
+init_image = download_image(img_url).resize((img_height, img_width))
+mask_img = download_image(mask_url).resize((img_height, img_width))
+
+device = 'cuda'
+
+# %%
+
+model_id = "stabilityai/stable-diffusion-2-1-base"
+pipe = StableDiffusionPipeline.from_pretrained(model_id).to(device)
+
+# %%
