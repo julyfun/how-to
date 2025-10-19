2fa985dc34264c38691efd52305c9f9d12265bf4
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2025-10-19 12:50:47 +0800

    M GitHub/github-test | M notes/julyfun/p1306-斐波那契公约数.cpp | M notes/julyfun/webapp

--- notes/julyfun/p1306-斐波那契公约数.cpp
+++ notes/julyfun/p1306-斐波那契公约数.cpp
@@ -4,19 +4,24 @@ int f[1010];
 int gcd(int a, int b) {
     return b == 0 ? a : gcd(b, a % b);
 }
-const int N = 25;
+const int N = 80;
 int main() {
     f[1] = f[0] = 1;
     for (int i = 2; i <= N; i++) {
         f[i] = (f[i - 1] + f[i - 2]) % int(1e8);
     }
-    for (int i = 0; i <= N; i++)
-        printf("%6d ", i);
     puts("");
     for (int i = 1; i <= N; i++, puts("")) {
-        printf("%6d ", i);
-        for (int j = 1; j <= N; j++)
-            printf("%6d ", gcd(f[i], f[j]));
+        printf("%3d", i);
+        for (int j = 1; j <= i; j++) {
+            int g = gcd(f[i], f[j]);
+            if (g % 5 == 0)
+                printf("*");
+            else
+                printf(" ");
+            // printf("%6d", gcd(f[i], f[j]) == 2 ? 2 : 0);
+            // printf
+        }
     }
     return 0;
 }
